466a1f58469249a7cf298f5fd554f7f6c2b0141e
smitchell1 <steve@platformconsulting.io>
2020-04-14 16:23:08 -0500

    Added mocking

--- api-gateway/src/gateway_api/routes.py
+++ api-gateway/src/gateway_api/routes.py
@@ -1,7 +1,7 @@
 import logging
 from flask import make_response, request, json, jsonify, abort
 from . import gateway_api_blueprint
-from .user_client import UserClient
+from .services.user_client import UserClient
 
 
 @gateway_api_blueprint.route('/api/gateway/version')
@@ -11,10 +11,14 @@ def get_version():
 
 @gateway_api_blueprint.route('/api/login', methods=['POST'])
 def login():
-    logging.debug('login()')
-    form = request.data  # data is empty
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if not username:
+        return make_response("Username parameter is missing", 400)
+    if not password:
+        return make_response("Password parameter is missing", 400)
     payload = {
-        'username': form.username.data,
-        'password': form.password.data,
+        'username': username,
+        'password': password,
     }
     return UserClient.post_login(payload)
---
+++ api-gateway/test/system/test_login.py
@@ -0,0 +1,53 @@
+from unittest.mock import MagicMock
+
+from flask import jsonify
+
+from base_test import BaseTest
+from gateway_api.services.user_client import UserClient
+from gateway_api import routes
+from mock import patch
+
+
+class TestLogin(BaseTest):
+
+    def test_get_login_405(self):
+        with self.app() as c:
+            resp = c.get('/api/login')
+            self.assertEqual(resp.status_code, 405)
+
+    def test_post_login_no_payload(self):
+        with self.app() as c:
+            resp = c.post('/api/login')
+            self.assertEqual(resp.status_code, 400)
+            self.assertEqual(resp.data.decode('utf-8'), 'Username parameter is missing')
+
+    def test_post_login_no_username(self):
+        with self.app() as c:
+            resp = c.post('/api/login',
+                          data=dict(password='a password'),
+                          follow_redirects=True
+                          )
+            self.assertEqual(resp.status_code, 400)
+            self.assertEqual(resp.data.decode('utf-8'), 'Username parameter is missing')
+
+    def test_post_login_no_password(self):
+        with self.app() as c:
+            resp = c.post('/api/login',
+                          data=dict(username='a user id'),
+                          follow_redirects=True
+                          )
+            self.assertEqual(resp.status_code, 400)
+            self.assertEqual(resp.data.decode('utf-8'), 'Password parameter is missing')
+
+    @patch('gateway_api.services.user_client.UserClient.post_login')
+    def test_post_login_mocked(self, post_login_mock):
+        with self.app() as c:
+            post_login_mock.return_value = 'abcdefg'
+            user_client = UserClient()
+            user_client.post_login = MagicMock(return_value='abcedef')
+            routes.user_client = user_client
+            resp = c.post('/api/login',
+                          data=dict(username='a user id', password='a password'),
+                          follow_redirects=True
+                          )
+            self.assertEqual(resp.status_code, 200)
